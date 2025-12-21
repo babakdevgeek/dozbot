7b54569bb5ddd44a4c181a8d7fed6973727ab869
Babak <Babakdevgeek@gmail.com>
2025-12-21 23:18:21 +0330

    "fix: bringing bot files to route"

--- api/telegram/route.js
+++ api/telegram/route.js
@@ -1,4 +1,10 @@
-import bot from "../../bot/bot";
+import { Telegraf } from "telegraf";
+import { message } from "telegraf/filters";
+
+const bot = new Telegraf(process.env.token);
+
+bot.start(ctx => ctx.reply("ربات فعاله"))
+bot.on(message("text"), ctx => ctx.reply("hi"));
 
 export default async function handler(req, res) {
     if (req.method === "POST") {
